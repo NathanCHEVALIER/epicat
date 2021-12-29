2eabc1bdb082db5f903e1775f7b415524cbcc210
UbuntuDell2 <nathan.chevalier@epita.fr>
2021-12-29 23:41:37 +0100

    [DATAS] - Add picture struct ans slices

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+// picture represents data about an epicat picture.
+type picture struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Cat string  `json:"cat"`
+    Url  float64 `json:"url"`
+}
+
+// pictures slice to seed data.
+var pictures = []picture{
+    {ID: "1", Title: "", Cat: "Hoppy", Url: ""},
+    {ID: "2", Title: "", Cat: "Hoppy", Url: ""},
+    {ID: "3", Title: "", Cat: "Hoppy", Url: ""},
+}
